115742176a631bc182c756f6545c74847ac8bb02
rnaryshkin <r.naryshkin99@gmail.com>
2024-10-03 01:22:47 +0100

    added draft for PlaylistService

---
+++ src/main/java/org/tera201/spotifymanagerapp/rest/outgoing/PlaylistService.java
@@ -0,0 +1,18 @@
+package org.tera201.spotifymanagerapp.rest.outgoing;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PlaylistService extends BaseService {
+
+    public void getPlaylist() {}
+
+    public void getUsersPlaylist() {}
+
+    public void getCurrentUserPlaylist() {}
+
+    public void addItemsToPlaylist() {}
+
+    public void createPlaylist() {}
+}
