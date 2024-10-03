b048345ab1b87b7f45e30ea8a4edb656af739631
rnaryshkin <r.naryshkin99@gmail.com>
2024-10-03 01:15:45 +0100

    added RestTemplate for requests

--- src/main/java/org/tera201/spotifymanagerapp/SpotifyManagerAppApplication.java
+++ src/main/java/org/tera201/spotifymanagerapp/SpotifyManagerAppApplication.java
@@ -2,9 +2,16 @@ package org.tera201.spotifymanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpotifyManagerAppApplication {
+
+    @Bean
+    public RestTemplate testRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpotifyManagerAppApplication.class, args);
